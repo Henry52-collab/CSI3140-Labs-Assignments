5a28302118bf337ae7ac9235df1c6c7f2623484f
Percy yiu <syiu017@uottawa.ca>
2024-02-17 22:39:41 -0500

    add name and StudentID, and some minor changes

--- A4/9.11.js
+++ A4/9.11.js
@@ -1,3 +1,8 @@
+// <!--Group 1-->
+// <!--Group Member:   Shun Hei, Yiu   300247812
+//                     Rui Heng, Tan   300102229-->
+
+// <!--Exercise 9.11-->
 // a)
 n =  Math.floor(Math.random() * 2) + 1;
 // console.log(n);
