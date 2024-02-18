8637678380e335984debb731c06e847b0866f76e
Percy yiu <syiu017@uottawa.ca>
2024-02-17 22:36:17 -0500

    finish 9.7, 9.11 a-c for A4 and 11.14 for L4

---
+++ A4/9.11.js
@@ -0,0 +1,9 @@
+// a)
+n =  Math.floor(Math.random() * 2) + 1;
+// console.log(n);
+// b)
+n =  Math.floor(Math.random() * 100) + 1;
+// console.log(n);
+// c)
+n =  Math.floor(Math.random() * 10);
+// console.log(n);
